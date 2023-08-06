a7ac1859c733ea8ec75e9e6c55bbda66682e20f5
MADUSHANKA <87461087+Madushankabro@users.noreply.github.com>
2023-08-06 18:37:04 +0530

    Updating........

---
+++ JavaScript/script.js
@@ -0,0 +1,36 @@
+var tablinks = document.getElementsByClassName("tab-links");
+        var tabcontents = document.getElementsByClassName("tab-contents");
+
+        function opentab(tabname) {
+            for (tablink of tablinks) {
+                tablink.classList.remove("active-link");
+            }
+
+            for (tabcontent of tabcontents) {
+                tabcontent.classList.remove("active-tab");
+            }
+            event.currentTarget.classList.add("active-link");
+            document.getElementById(tabname).classList.add("active-tab");
+ }
+
+ var sidemenu=document.getElementById("sidemenu");
+
+        function openmenu(){
+            sidemenu.style.right="0";
+        }
+        
+        function closemenu(){
+            sidemenu.style.right="-200px";
+}
+
+const toTop = document.querySelector(".to-top");
+
+  window.addEventListener("scroll", () =>{
+    if(window.pageYOffset >100){
+        toTop.classList.add("active");
+    }
+    else{
+        toTop.classList.remove("active")
+    }
+  })
+  
